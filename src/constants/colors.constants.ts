8ecee693b9731725e900726494d00393f058f8ed
vakadu11 <vinod@arzooo.com>
2023-09-07 12:06:38 +0530

    added new build

---
+++ src/constants/colors.constants.ts
@@ -0,0 +1,8 @@
+export const CustomColors = {
+	primary: '#3a9545',
+	secondary: '#000000',
+	onSecondary: '#FFFFFF',
+	error: '#B03A2E',
+	onError: '#FFFFFF',
+	tertiary: '#D7DBDD',
+};
---
+++ src/constants/font-varaints.constants.ts
@@ -0,0 +1,44 @@
+import { FontGilroy } from '@primitives/index';
+
+export const BaseFont = {
+    fontFamily: FontGilroy.Regular,
+} as const;
+
+export const CustomFontVariants = {
+	labelSmall: {
+		fontFamily: FontGilroy.SemiBold,
+		fontWeight: '600',
+		fontSize: 12,
+		letterSpacing: 0,
+	},
+    labelMedium: {
+		fontFamily: FontGilroy.SemiBold,
+		fontWeight: '600',
+		fontSize: 14,
+		letterSpacing: 0,
+	},
+	labelLarge: {
+		fontFamily: FontGilroy.SemiBold,
+		fontWeight: '600',
+		fontSize: 16,
+		letterSpacing: 0,
+	},
+	titleSmall: {
+		fontFamily: FontGilroy.Regular,
+		fontWeight: '300',
+		fontSize: 12,
+		letterSpacing: 0,
+	},
+    titleMedium: {
+		fontFamily: FontGilroy.Regular,
+		fontWeight: '400',
+		fontSize: 14,
+		letterSpacing: 0,
+	},
+	titleLarge: {
+		fontFamily: FontGilroy.Regular,
+		fontWeight: '500',
+		fontSize: 16,
+		letterSpacing: 0,
+	},
+} as const;
